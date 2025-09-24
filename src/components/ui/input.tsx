7ffad06b2e568fa9c07436c7fd84f20a0a06f3c4
Brennan (Brenntron) Willingham <brennan.willingham@gmail.com>
2025-09-24 11:29:18 -0500

    Fix searchbar spacing to account for svg.

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { clsx } from "clsx";
+import "@/styles/input.css";
 
 export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
@@ -9,7 +10,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={clsx(
-          "flex h-10 w-full rounded-md border border-gray-200 bg-white px-3 py-2 text-sm ring-offset-whitie fiile:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-gray-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-0ffset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          "searchbar flex h-10 w-full rounded-md border border-gray-200 bg-white px-3 py-2 text-sm ring-offset-whitie fiile:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-gray-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-0ffset-2 disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
         ref={ref}
